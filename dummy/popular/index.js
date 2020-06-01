a31e79010b78d4f9900a181ddc7286ff42b9ca8d
조연우 <wooisso@naver.com>
2020-06-01 21:27:38 +0900

    Create Dummy Api GET /popular

---
+++ dummy/popular/index.js
@@ -0,0 +1,26 @@
+[
+    {
+        "no" : 1,
+        "title" : "Shut up and Squat!",
+        "category" : "Morning Breaker",
+        "thumbnail" : "https://firebasestorage.googleapis.com/v0/b/lemorning-bf1e1.appspot.com/o/dev%2Fimages%2Fimg_album_sample1_kim.png?alt=media&token=f814bfe8-8de4-49a9-be42-aa6b15dd02e1",
+        "music" : "",
+        "userLiked" : true
+    },
+    {
+        "no" : 2,
+        "title" : "KKang - Rain",
+        "category" : "Morning Breaker",
+        "thumbnail" : "https://firebasestorage.googleapis.com/v0/b/lemorning-bf1e1.appspot.com/o/dev%2Fimages%2Fimg_album_sample2_kkang.png?alt=media&token=98f15cb2-f182-4546-b8cb-8381e5a8c654",
+        "music" : "",
+        "userLiked" : false
+    },
+    {
+        "no" : 3,
+        "title" : "Hell o",
+        "category" : "Morning Breaker",
+        "thumbnail" : "https://firebasestorage.googleapis.com/v0/b/lemorning-bf1e1.appspot.com/o/dev%2Fimages%2Fimg_album_sample3_gwan.png?alt=media&token=d8280e7b-2734-4816-aaee-5c3e17ac4bb1",
+        "music" : "",
+        "userLiked" : true
+    }
+]
